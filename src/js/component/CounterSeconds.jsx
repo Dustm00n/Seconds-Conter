2759e18339438766d4fde1570106185fd814d3c4
Dustm00n <gerardoserranosoto@gmail.com>
2022-02-04 20:35:24 +0000

    Avance falta ponerlo bonito

---
+++ src/js/component/CounterSeconds.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+const SecondsCounter = (props) => {
+	return (
+		<div className="main-counter">
+			<div className="row">
+				<div className="col">
+					<div className="clock-calendar">
+						<i className="far fa-clock"></i>
+					</div>
+					<div className="Seis">{props.digitSeis % 10}</div>
+					<div className="Cinco">{props.digitCinco % 10}</div>
+					<div className="Cuatro">{props.digitCuatro % 10}</div>
+					<div className="Tres">{props.digitTres % 10}</div>
+					<div className="Dos">{props.digitDos % 10}</div>
+					<div className="Uno">{props.digitUno % 10}</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+SecondsCounter.propTypes = {
+	digitSeis: PropTypes.number,
+	digiCinco: PropTypes.number,
+	digitCuatro: PropTypes.number,
+	digitTres: PropTypes.number,
+	digitDos: PropTypes.number,
+	digitUno: PropTypes.number,
+};
+export default SecondsCounter;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,30 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+import SecondsCounter from "./CounterSeconds.jsx";
+
+let countingNumber = 0;
+setInterval(function () {
+	let Seis = Math.floor(countingNumber / 100000);
+	let Cinco = Math.floor(countingNumber / 10000);
+	let Cuatro = Math.floor(countingNumber / 1000);
+	let Tres = Math.floor(countingNumber / 100);
+	let Dos = Math.floor(countingNumber / 10);
+	let Uno = Math.floor(countingNumber / 1);
+	countingNumber++;
+	ReactDOM.render(
+		<SecondsCounter
+			digitUno={Uno}
+			digitDos={Dos}
+			digitTres={Tres}
+			digitCuatro={Cuatro}
+			digitCinco={Cinco}
+			digitSeis={Seis}
+		/>,
+		document.querySelector("#app")
 	);
-};
+}, 1000);
 
 export default Home;
