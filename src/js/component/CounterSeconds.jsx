7fb81258a07da6874eb43c46c561a91ba85d3d90
Dustm00n <gerardoserranosoto@gmail.com>
2022-02-07 20:51:40 +0000

    arreglo entre comillas

--- src/js/component/CounterSeconds.jsx
+++ src/js/component/CounterSeconds.jsx
@@ -2,19 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 const SecondsCounter = (props) => {
 	return (
-		<div className="main-counter">
-			<div className="row">
-				<div className="col">
-					<div className="clock-calendar">
-						<i className="far fa-clock"></i>
-					</div>
-					<div className="Seis">{props.digitSeis % 10}</div>
-					<div className="Cinco">{props.digitCinco % 10}</div>
-					<div className="Cuatro">{props.digitCuatro % 10}</div>
-					<div className="Tres">{props.digitTres % 10}</div>
-					<div className="Dos">{props.digitDos % 10}</div>
-					<div className="Uno">{props.digitUno % 10}</div>
+		<div className="Caja bg-dark">
+			<div className="cajita2 main-cotainer d-flex justify-content-center">
+				<div className="clock-calendar">
+					<i className="far fa-clock"></i>
 				</div>
+				<div className="Seis">{props.digitSeis % 10}</div>
+				<div className="Cinco">{props.digitCinco % 10}</div>
+				<div className="Cuatro">{props.digitCuatro % 10}</div>
+				<div className="Tres">{props.digitTres % 10}</div>
+				<div className="Dos">{props.digitDos % 10}</div>
+				<div className="Uno">{props.digitUno % 10}</div>
 			</div>
 		</div>
 	);
